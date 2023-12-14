cd65d01356b9e0631e7b2aeb007142b4d2789d3c
templeajuzie <templeajuzie@gmail.com>
2023-12-14 08:07:59 +0100

    server fixed

--- server.js
+++ server.js
@@ -1,80 +1,69 @@
 const express = require("express");
-require("dotenv").config();
+const http = require("http");
+const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const connectDb = require("./db/ConnectDb");
 const blogRouter = require("./routes/blogRoutes");
 const authRouter = require("./routes/authRoutes");
-const bodyParser = require("body-parser");
 const multer = require("multer");
-
-const path = require("path");
-const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const {
-  handleNewComment,
-  postReaction,
-} = require("./controllers/blogControllers");
+const { handleNewComment, postReaction } = require("./controllers/blogControllers");
 const { userConnect } = require("./controllers/authControllers");
-
-const cookieParser = require("cookie-parser");
-
-const clientUrl = process.env.CLIENT_URL;
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: [clientUrl],
+    origin: [process.env.CLIENT_URL],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
   },
 });
 
-//sockets
-
-handleNewComment(io);
-postReaction(io);
-userConnect(io);
-
-app.use(
-  cors({
-    origin: [clientUrl],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    credentials: true,
-  })
-
-);
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-connectDb(server);
+// Middleware
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Authorization", "Content-Type"],
+  credentials: true,
+}));
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// Uncomment if file upload middleware is needed
 // app.use(fileUpload({ useTempFiles: true }));
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
-    // Multer error occurred
-    console.log("error");
     res.status(400).send("Multer error: " + err.message);
   } else {
-    // Handle other errors
-    console.log("next");
+    console.error("Other error:", err);
     next(err);
   }
 });
 
+// Routes
 app.use("/api/v1/blog", blogRouter);
 app.use("/api/v1/auth", authRouter);
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+// Socket.IO Events
+handleNewComment(io);
+postReaction(io);
+userConnect(io);
+
+// Start the server
+connectDb(server);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
